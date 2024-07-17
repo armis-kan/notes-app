c16541e2e4c0e7bffb328fb3f617dacff2dfd9cc
Armis Kantarević <ak53739@fer.hr>
2024-07-18 01:44:48 +0200

    Fixing errors about connecting to the db

--- server/db.js
+++ server/db.js
@@ -35,7 +35,7 @@ const getUserByUsername = async (username) => {
 };
 
 const getUserById = async (id) => {
-    const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+    const result = await pool.query('SELECT * FROM users WHERE user_id = $1', [id]);
     return result.rows[0];
 };
 
